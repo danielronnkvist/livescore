d8948bb6bdcd309ac194b49d6fbe50314ce1bbbf
danielronnkvist <daniel@ronnkvist.com>
2014-11-06 22:20:46 +0100

    Added filtering

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,21 @@ var app = angular.module('live', []);
 app.controller('view', function ($scope, $http){
   $http.get('/info').
     success(function (data, status, headers, config){
-      $scope.games = data;
+      if(data != "false")
+        $scope.games = data;
+        $scope.leagues = [];
+        for(var i = 0; i < $scope.games.length; i++)
+        {
+          if($scope.leagues.indexOf($scope.games[i].league) < 0)
+            $scope.leagues.push($scope.games[i].league);
+        }
     });
+
   setInterval(function(){
     $http.get('/info').
       success(function (data, status, headers, config){
-        $scope.games = data;
+        if(data != "false")
+          $scope.games = data;
       });
   }, 21000);
 });
