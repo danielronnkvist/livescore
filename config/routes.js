b08f6e4687209b068b064aaadd9f5e16833c7e7c
danielronnkvist <daniel@ronnkvist.com>
2014-11-07 11:28:12 +0100

    Angular templates, index only

--- config/routes.js
+++ config/routes.js
@@ -2,5 +2,6 @@ var pages = require('../app/controllers/pages.js');
 
 // Start page
 app.get('/', pages.index);
+app.get('/Match/:id', pages.index)
 
 app.get('/info', pages.info);
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,22 @@
-var app = angular.module('live', []);
+var app = angular.module('live', ['ngRoute']);
 
-app.controller('view', function ($scope, $http){
-  $http.get('/info').
-    success(function (data, status, headers, config){
+app.config(function ($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: '/templates/list.html',
+      controller: 'MainCtrl'
+    })
+    .when('/Match/:matchId', {
+      templateUrl: '/templates/match.html',
+      controller: 'MatchCtrl'
+    });
+});
+
+app.controller('MainCtrl', function ($scope, $http){
+  $http.get('/info')
+    .success(function (data, status, headers, config){
       if(data != "false")
+      {
         $scope.games = data;
         $scope.leagues = [];
         for(var i = 0; i < $scope.games.length; i++)
@@ -11,13 +24,22 @@ app.controller('view', function ($scope, $http){
           if($scope.leagues.indexOf($scope.games[i].league) < 0)
             $scope.leagues.push($scope.games[i].league);
         }
+      }
     });
 
   setInterval(function(){
-    $http.get('/info').
-      success(function (data, status, headers, config){
+    $http.get('/info')
+      .success(function (data, status, headers, config){
         if(data != "false")
           $scope.games = data;
       });
   }, 21000);
+});
+
+app.controller('MatchCtrl', function ($scope, $http){
+  $http.get('/info')
+    .success(function (data, status, headers, config){
+      if(data != "false")
+        $scope = data[$routeParams];
+    });
 });
